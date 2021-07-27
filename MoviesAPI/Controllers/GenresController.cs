d5cb7c9e04edbe41929340dd1caa6c1d67095f2e
girdhar02 <girdharsahu02@gmail.com>
2021-07-27 16:58:09 +0530

    Add project files.

---
+++ MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using MoviesAPI.Entities;
+using MoviesAPI.Filters;
+using MoviesAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+
+    public class GenresController : ControllerBase
+    {
+        private readonly IRepository repository;
+        private readonly ILogger<GenresController> logger;
+
+        public GenresController(IRepository repository,ILogger<GenresController> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+        [HttpGet] //"api/genre"
+        [HttpGet("list")] //"api/genre/list"
+        //[ResponseCache(Duration =60)]
+        [ServiceFilter(typeof(MyActionFilter))]
+        public async Task<ActionResult<List<Genre>>> Get()
+        {
+            logger.LogInformation("Getting all the Genres");
+            return await repository.GetAllGenres();
+        }
+
+        [HttpGet("{Id:int}",Name ="getGenre")] //"api/genres/Id"  
+        public ActionResult<Genre> Get(int Id, [FromHeader] string pram2)
+        {
+            logger.LogDebug("GetById method executing...");
+            var genre = repository.GetGenreById(Id);
+            if(genre == null)
+            {
+                logger.LogWarning($"Genre with {Id} not found");
+                throw new ApplicationException();
+                return NotFound();
+            }
+
+            return genre;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genre genre)
+        {
+            repository.AddGenre(genre);
+            return new CreatedAtRouteResult("getGenre", new { Id = genre.Id }, genre);
+        }
+        
+        [HttpPut]
+        public ActionResult Put([FromBody] Genre genre)
+        {
+            return NoContent();
+        }
+        
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ MoviesAPI/Entities/Genre.cs
@@ -0,0 +1,40 @@
+﻿using MoviesAPI.Controllers;
+using MoviesAPI.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Entities
+{
+    public class Genre //: IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="The Field with name '{0}' is required")]
+        [StringLength(10)]
+        [FirstLetterUpperCaseAttribute]
+        public string Name { get; set; }
+
+        //  [Range(18,120)]
+        //public int age { get; set; }
+        //[CreditCard]
+        //public string CreditCard { get; set; }
+        //[Url]
+        //public string Url { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validation)
+        //{
+        //    if (!string.IsNullOrEmpty(Name))
+        //    {
+        //        var firstLetter = Name[0].ToString();
+        //        if(firstLetter != firstLetter.ToUpper())
+        //        {
+        //            yield return new ValidationResult("First letter should be uppercase", new string[] { nameof(Name) });
+        //        }
+        //    }
+        //}
+    
+    }
+}
